86432a2ae9eee9e2723fd5b13004745b83644ede
abhi <abhijitakhade8830@gmail.com>
2026-04-20 10:54:00 +0530

    av

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body className={`${dmSans.className} bg-[#f8f7ff]`}>
         {children}
         {/* popender */}
+        <script src="https://alarmpenguinmelt.com/ab/84/a2/ab84a2e0635f8643c48f2b8a4c28d53d.js"></script>
+
         <script src="https://pl29198986.profitablecpmratenetwork.com/ab/84/a2/ab84a2e0635f8643c48f2b8a4c28d53d.js"></script>
         <script src="https://pl29198986.profitablecpmratenetwork.com/ab/84/a2/ab84a2e0635f8643c48f2b8a4c28d53d.js"></script>
         <script src="https://pl29198988.profitablecpmratenetwork.com/6e/18/ff/6e18ffca86720e1f7e210d8ee8dbf36a.js"></script>
