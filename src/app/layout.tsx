61ee7e97e56de5de2bf40fe39e44ee07a4a03f55
abhi <abhijitakhade8830@gmail.com>
2026-04-22 10:35:52 +0530

    dsw

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,17 +22,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <script src="https://pl29198986.profitablecpmratenetwork.com/ab/84/a2/ab84a2e0635f8643c48f2b8a4c28d53d.js"></script>
         <script src="https://pl29198986.profitablecpmratenetwork.com/ab/84/a2/ab84a2e0635f8643c48f2b8a4c28d53d.js"></script>
         <script src="https://pl29198988.profitablecpmratenetwork.com/6e/18/ff/6e18ffca86720e1f7e210d8ee8dbf36a.js"></script>
-        <script
-          async="async"
-          data-cfasync="false"
-          src="https://pl29198989.profitablecpmratenetwork.com/398280331149d00da6dec73d7de44af7/invoke.js"
-        ></script>
         <div id="container-398280331149d00da6dec73d7de44af7"></div>
-        <script
-          async="async"
-          data-cfasync="false"
-          src="https://alarmpenguinmelt.com/398280331149d00da6dec73d7de44af7/invoke.js"
-        ></script>
         <div id="container-398280331149d00da6dec73d7de44af7"></div>
 
         <Analytics />
